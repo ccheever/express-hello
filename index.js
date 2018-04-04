a18937a53c1f6ca59395c083640375da95f943fd
Charlie Cheever <ccheever@gmail.com>
2018-04-04 14:59:06 -0700

    Adding files

---
+++ index.js
@@ -0,0 +1,8 @@
+let express = require('express')
+let app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log('Example app listening on port ' + port + '!'))
